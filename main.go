178f8de9ce8faf861f3a87451517cc24e8debdef
Philip Reinken <p.reinken@mailbox.org>
2024-03-15 00:08:06 +0100

    Add pipeline functions

--- main.go
+++ main.go
@@ -3,21 +3,24 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	oci "github.com/opencontainers/image-spec/specs-go/v1"
 	"strings"
 	"time"
 )
 
+const wavelogRepoUrl = "https://github.com/wavelog/wavelog.git"
+
 type WavelogDocker struct{}
 
-func (m *WavelogDocker) ListWavelogGitTags(ctx context.Context) (JSON, error) {
+func listTags(ctx context.Context, repository string) (map[string]string, error) {
 	tagsString, err := dag.Container().
 		From("bitnami/git:2").
-		WithDefaultArgs([]string{"git", "ls-remote", "--tags", "https://github.com/wavelog/wavelog.git"}).
+		WithDefaultArgs([]string{"git", "ls-remote", "--tags", repository}).
 		Stdout(ctx)
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	tags := make(map[string]string)
@@ -30,11 +33,15 @@ func (m *WavelogDocker) ListWavelogGitTags(ctx context.Context) (JSON, error) {
 		sha := s[0]
 
 		tags[tag] = sha
+	}
 
-		// TODO: Work out a proper way to tag the latest build as latest
-		//if i >= len(tagLines)-1 {
-		//	tags["latest"] = sha
-		//}
+	return tags, nil
+}
+
+func (m *WavelogDocker) ListWavelogGitTags(ctx context.Context) (JSON, error) {
+	tags, err := listTags(ctx, wavelogRepoUrl)
+	if err != nil {
+		return "", err
 	}
 
 	ret, err := json.Marshal(tags)
@@ -59,12 +66,79 @@ func (m *WavelogDocker) AddLabels(c *Container) *Container {
 }
 
 // Builds the container as specified in docker/Dockerfile
-func (m *WavelogDocker) Build(ctx context.Context, wavelogVersion string, phpVersion string) *Container {
+func (m *WavelogDocker) Build(
+	ctx context.Context,
+	// +optional
+	container *Container,
+	wavelogVersion string,
+	phpVersion string,
+) *Container {
 	opts := ContainerBuildOpts{Dockerfile: "Dockerfile", BuildArgs: []BuildArg{}}
 
 	opts.BuildArgs = append(opts.BuildArgs, BuildArg{Name: "PHP_VERSION", Value: phpVersion})
 	opts.BuildArgs = append(opts.BuildArgs, BuildArg{Name: "WAVELOG_VERSION", Value: wavelogVersion})
 
-	return m.AddLabels(dag.Container()).
+	if container == nil {
+		container = dag.Container()
+	}
+
+	return m.AddLabels(container).
 		Build(dag.CurrentModule().Source().Directory("docker"), opts)
+}
+
+func (m *WavelogDocker) BuildPipeline(ctx context.Context) ([]*Container, error) {
+	build := dag.Pipeline("build")
+	wavelogTags, err := listTags(ctx, wavelogRepoUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(wavelogTags) < 1 {
+		return nil, fmt.Errorf("No tags found in repository %s!", wavelogRepoUrl)
+	}
+
+	var containers []*Container
+
+	for tag, sha := range wavelogTags {
+		pipelinedContainer := build.Pipeline("wavelog").Pipeline(tag).Container().
+			WithLabel(oci.AnnotationVersion, tag).
+			WithLabel(oci.AnnotationRevision, sha)
+
+		containers = append(containers, m.Build(ctx, pipelinedContainer, tag, "8.3"))
+	}
+
+	return containers, nil
+}
+
+func (m *WavelogDocker) PublishPipeline(ctx context.Context) error {
+	publish := dag.Pipeline("publish")
+	containers, err := m.BuildPipeline(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, container := range containers {
+		id, err := container.ID(ctx)
+		if err != nil {
+			return err
+		}
+
+		response, err := publishGhcr(ctx, publish.LoadContainerFromID(id))
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(response)
+	}
+
+	return nil
+}
+
+func publishGhcr(ctx context.Context, container *Container) (string, error) {
+	version, err := container.Label(ctx, oci.AnnotationVersion)
+	if err != nil {
+		return version, err
+	}
+
+	return container.Publish(ctx, fmt.Sprintf("ghcr.io/philipreinken/wavelog:%s", version))
 }
